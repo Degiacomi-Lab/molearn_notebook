773269058b6d426a8b298b0338ded54ca0a4584f
DEGIACOMI <degiacom@users.noreply.github.com>
2023-03-28 15:58:55 +0100

    moved analysis.py to molearn

--- scoring/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Scoring holds the classes for calculating DOPE, Ramachandran and OPENMM energy
-"""
-from .dope_score import Parallel_DOPE_Score, DOPE_Score
-from .ramachandran_score import Parallel_Ramachandran_Score, Ramachandran_Score
